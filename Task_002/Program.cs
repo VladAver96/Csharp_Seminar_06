19b0b81edfa6d39f60ffff74af74761f973b963c
Vlad Aver <averyanau19gmail.com>
2023-12-09 14:20:29 +0100

    2 pflfxf

---
+++ Task_002/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Введите строку с латинскими буквами в обоих регистрах
+        Console.Write("Введите строку: ");
+        string inputString = Console.ReadLine();
+
+        // Формируем новую строку с заменой заглавных букв на строчные
+        string resultString = ConvertToLowerCase(inputString);
+
+        // Выводим результат
+        Console.WriteLine($"Исходная строка: {inputString}");
+        Console.WriteLine($"Результат: {resultString}");
+    }
+
+    // Функция для замены заглавных букв на строчные
+    static string ConvertToLowerCase(string input)
+    {
+        // Используем метод ToLower для замены заглавных букв на строчные
+        return input.ToLower();
+    }
+}
+
